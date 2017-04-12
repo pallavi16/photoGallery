b64e40b6a9c947c88f7c0cfa08df898df83272bb
Pallavi Aggarwal <pallaviaggarwal@Pallavis-MacBook-Pro.local>
2017-04-12 17:11:33 -0600

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  
+  before_filter :getCategoryNav	
+  protect_from_forgery with: :exception
+
+private
+	def getCategoryNav
+		@categoryNav = Category.all
+	end	
+end
